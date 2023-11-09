cd4a59e3a7230a1f0ff1fce8d5832a6d984f4255
CodeZhangBorui <zhangborui.2008@foxmail.com>
2023-11-09 08:53:54 +0800

    feat(gencfrate): 更新 Codeforces 题目爬取器并更新最新题库

---
+++ data/gencfrate.py
@@ -0,0 +1,31 @@
+# Generate cfrate.json from codeforces.com
+
+import json
+import requests
+
+data = requests.get("https://codeforces.com/api/problemset.problems").json()
+
+output = {}
+
+for problem in data["result"]["problems"]:
+    try:
+        rating = problem["rating"]
+    except KeyError:
+        continue
+    try:
+        output[rating].append({
+            "pid": f"{problem['contestId']}{problem['index']}",
+            "name": problem['name'],
+            "url": f"https://codeforces.com/contest/{problem['contestId']}/problem/{problem['index']}"
+        })
+    except KeyError:
+        output[rating] = [
+            {
+                "pid": f"{problem['contestId']}{problem['index']}",
+                "name": problem['name'],
+                "url": f"https://codeforces.com/contest/{problem['contestId']}/problem/{problem['index']}"
+            }
+        ]
+
+with open("cfrate.json", "w", encoding='utf-8') as f:
+    json.dump(output, f)
